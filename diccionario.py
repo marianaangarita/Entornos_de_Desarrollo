7e1609fcf2281b87123a5626e846aa5149caf845
marianaangarita <mariana.angarita.afonso@gmail.com>
2026-02-18 20:34:15 +0100

    crear lista palabras

---
+++ diccionario.py
@@ -0,0 +1,66 @@
+abecedario=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+lista_palabras = [
+    # A
+    "abeja", "árbol", "avión",
+    # B
+    "balón", "barco", "bota",
+    # C
+    "cama", "casa", "coche",
+    # D
+    "dado", "dedo", "diente",
+    # E
+    "elefante", "estrella", "escuela",
+    # F
+    "foca", "fuego", "fruta",
+    # G
+    "gato", "gorra", "guante",
+    # H
+    "hielo", "huevo", "hoja",
+    # I
+    "iglesia", "iglú", "isla",
+    # J
+    "jabón", "jardín", "jirafa",
+    # K
+    "karate", "kilo", "koala",
+    # L
+    "lápiz", "libro", "luna",
+    # M
+    "mano", "mesa", "mono",
+    # N
+    "nariz", "nieve", "nube",
+    # O
+    "ojo", "oreja", "oso",
+    # P
+    "pato", "perro", "piña",
+    # Q
+    "queso", "quiosco", "química",
+    # R
+    "rana", "ratón", "reloj",
+    # S
+    "sol", "silla", "sopa",
+    # T
+    "taza", "tigre", "tren",
+    # U
+    "uno", "uva", "uña",
+    # V
+    "vaca", "vaso", "vela",
+    # W
+    "waffle", "web", "wifi",
+    # X
+    "xenón", "xilófono", "xilografía",
+    # Y
+    "yate", "yema", "yoyó",
+    # Z
+    "zanahoria", "zapato", "zorro"
+]
+
+# Verificación rápida (opcional)
+print(f"Total de palabras: {len(lista_palabras)}")
+print(lista_palabras)
+diccionario={}
+
+for letra in abecedario:
+    diccionario[letra]=[]
+
+print(diccionario)
+
